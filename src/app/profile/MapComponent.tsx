a825586536de91977b93d3d8d4ef2c170b66f5c3
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2024-10-07 14:21:26 +0100

    trying ssr again

--- src/app/profile/MapComponent.tsx
+++ src/app/profile/MapComponent.tsx
@@ -1,27 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import dynamic from "next/dynamic";
 import axios from "axios";
 
-// Dynamically import Leaflet without SSR
-const MapContainer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.MapContainer),
-  { ssr: false }
-);
-const TileLayer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.TileLayer),
-  { ssr: false }
-);
-const Marker = dynamic(
-  () => import("react-leaflet").then((mod) => mod.Marker),
-  { ssr: false }
-);
-import "leaflet/dist/leaflet.css";
 
-// Define the component
+const leafletCssUrl = "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css";
+
 const MapComponent: React.FC<{
   onSelectAddress: (address: string) => void;
 }> = ({ onSelectAddress }) => {
-  const [position, setPosition] = useState<[number, number]>([48.8566, 2.3522]); // Default to Paris
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = leafletCssUrl;
+    document.head.appendChild(link);
+  }, []);
+
+  const [position, setPosition] = useState<[number, number]>([48.8566, 2.3522]);
+
+  const LocationMarker = () => {
+    useMapEvents({
+      click(e) {
+        setPosition([e.latlng.lat, e.latlng.lng]); 
+        fetchAddress(e.latlng.lat, e.latlng.lng); 
+      },
+    });
+
+    return <Marker position={position} />;
+  };
 
   const fetchAddress = async (lat: number, lon: number) => {
     try {
@@ -34,18 +40,6 @@ const MapComponent: React.FC<{
     } catch (error) {
       console.error("Error fetching reverse address:", error);
     }
-  };
-
-  const LocationMarker = () => {
-    // Handle map click events to get the clicked location's coordinates
-    const map = useMapEvents({
-      click(e) {
-        setPosition([e.latlng.lat, e.latlng.lng]);
-        fetchAddress(e.latlng.lat, e.latlng.lng);
-      },
-    });
-
-    return <Marker position={position} />;
   };
 
   return (
