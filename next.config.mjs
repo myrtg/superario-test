ad0edc82f93611a964a95395cd29980ad98a1073
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2024-10-07 14:48:20 +0100

    ssss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 import withTM from "next-transpile-modules";
 
 const nextConfig = withTM(["react-leaflet", "leaflet"])({
-  reactStrictMode: true, // You can keep other Next.js configurations here
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false, // Leaflet sometimes needs these fallbacks
+      path: false, // Avoid Webpack issues with node modules
+    };
+    return config;
+  },
+  reactStrictMode: true,
 });
 
 export default nextConfig;
