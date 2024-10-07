a90c577066ffb042813c1aa7d546d76f0de69377
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2024-10-07 11:28:42 +0100

    Fix dynamic import for react-leaflet

--- src/app/profile/MapComponent.tsx
+++ src/app/profile/MapComponent.tsx
@@ -1,35 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import dynamic from "next/dynamic";
 import axios from "axios";
 
-// Dynamically import Leaflet CSS to avoid SSR issues
-const leafletCssUrl = "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css";
+// Dynamically import Leaflet without SSR
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+import "leaflet/dist/leaflet.css";
 
-// Ensure leaflet's CSS is loaded on the client side
+// Define the component
 const MapComponent: React.FC<{
   onSelectAddress: (address: string) => void;
 }> = ({ onSelectAddress }) => {
-  useEffect(() => {
-    // Dynamically load Leaflet CSS
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = leafletCssUrl;
-    document.head.appendChild(link);
-  }, []);
-
   const [position, setPosition] = useState<[number, number]>([48.8566, 2.3522]); // Default to Paris
-
-  const LocationMarker = () => {
-    useMapEvents({
-      click(e) {
-        setPosition([e.latlng.lat, e.latlng.lng]); // Update position on click
-        fetchAddress(e.latlng.lat, e.latlng.lng); // Fetch address based on position
-      },
-    });
-
-    return <Marker position={position} />;
-  };
 
   const fetchAddress = async (lat: number, lon: number) => {
     try {
@@ -38,10 +30,22 @@ const MapComponent: React.FC<{
       );
       const address =
         response.data.features[0]?.properties?.label || "Unknown Address";
-      onSelectAddress(address); // Call parent component's callback to pass the address
+      onSelectAddress(address);
     } catch (error) {
       console.error("Error fetching reverse address:", error);
     }
+  };
+
+  const LocationMarker = () => {
+    // Handle map click events to get the clicked location's coordinates
+    const map = useMapEvents({
+      click(e) {
+        setPosition([e.latlng.lat, e.latlng.lng]);
+        fetchAddress(e.latlng.lat, e.latlng.lng);
+      },
+    });
+
+    return <Marker position={position} />;
   };
 
   return (
