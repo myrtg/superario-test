7d094a7ccdcae49b24f0bb9b7293fa73314e666a
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2024-10-07 14:45:32 +0100

    trying transpile modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withTM from "next-transpile-modules";
+
+const nextConfig = withTM(["react-leaflet", "leaflet"])({
+  reactStrictMode: true, // You can keep other Next.js configurations here
+});
 
 export default nextConfig;
